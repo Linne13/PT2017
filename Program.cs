20cc4ca69ecc2479f735278838627e39878acec5
Alex <a_kiddo13@hotmail.com>
2017-02-10 11:51:53 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int sum = 0;
+            int a;
+
+            while (n != 0)
+            {
+                a = n % 10;
+                n = n / 10;
+                sum = a + sum;
+                Console.WriteLine(n);
+            }
+          
+            Console.Write(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
