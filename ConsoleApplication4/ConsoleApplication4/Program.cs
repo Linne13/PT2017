313f8a7959fc488ca18850f922f23afb5a65532b
Linne13 <a_kiddo13@hotmail.com>
2017-02-14 22:33:55 +0600

    MaxMin

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int max = File.ReadAllLines("numbers.txt").Select(int.Parse).Max();
+             int min = File.ReadAllLines("numbers.txt").Select(int.Parse).Min();
+
+             Console.WriteLine("Maximum:" + " " + max);
+             Console.WriteLine("Minimum:" + " " + min);
+             Console.ReadKey(); 
+          
+        }
+    }
+}
