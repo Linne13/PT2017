e90768cbb9a725abe66d70ee01e1f5403c70d450
Linne13 <a_kiddo13@hotmail.com>
2017-03-11 07:00:51 +0600

    lab 2-4

--- lab2-4/lab2-4/Program.cs
+++ lab2-4/lab2-4/Program.cs
@@ -6,60 +6,68 @@ using System.Threading.Tasks;
 
 namespace lab2_4
 {
-    public class Complex
+    class Program
     {
-        public int a;
-        public int b;
-
-        public Complex()
+        class Complex
         {
-            a = 0;
-            b = 0;
-
-        }
-
-        public Complex(int a, int b)
-        {
-            this.a = a;
-            this.b = b;
-
-        }
-        public static Complex operator +(Complex c1, Complex c2)
-        {
-            Complex c = new Complex(0, 0);
-
-            c.a = c1.a + c2.a;
-            c.b = c1.b + c2.b;
-
-            return c;
-        }
-        public override string ToString()
-        {
-            return this.a + " " + this.b + "*i";
-        }
-    }
-
-
-    static void Main(string[] args)
-    {
-        List<Complex> list = new List<Complex>();
-        while (true)
-        {
-
-            int w = int.Parse(Console.ReadLine());
-            int l = int.Parse(Console.ReadLine());
-
-            Complex a = new Complex(w, l);
-            list.Add(a);
-
-            foreach (Complex k in list)
+            public int x, y;
+            public Complex(int x, int y)
             {
-                Console.WriteLine(k.ToString());
+                this.x = x;
+                this.y = y;
             }
-            Console.WriteLine(a + a);
 
-            int p = int.Parse(Console.ReadLine());
-            if (p == 0) { break; }
+            public void Print()
+            {
+                Console.WriteLine(x + "/" + y);
+            }
+
+
+            public override string ToString()
+            {
+                return x + "/" + y;
+            }
+
+
+            public Complex Add(Complex c)
+            {
+                Complex d = new Complex(this.x * c.y + c.x * this.y, this.y * c.y);
+                return d;
+            }
+
+            public static Complex operator +(Complex c1, Complex c2)
+            {
+                Complex c = new Complex(c1.x * c2.y + c2.x * c1.y, c1.y * c2.y);
+                return c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Complex> list = new List<Complex>();
+            while (true)
+            {
+
+                int w = int.Parse(Console.ReadLine());
+                int l = int.Parse(Console.ReadLine());
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+
+                Complex a = new Complex(w, l);
+                list.Add(a);
+                Complex b = new Complex(x, y);
+                list.Add(b);
+
+
+                Console.WriteLine(a + b);
+
+
+            }
+            /*Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 3);
+            Complex c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();*/
         }
     }
 }
