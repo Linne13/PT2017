f462293f7506e73e71adb9309781535fc6b7ee00
Alex <a_kiddo13@hotmail.com>
2017-05-02 14:38:27 +0600

    mooving

---
+++ moovingasteroids/moovingasteroids/Asteroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace moovingasteroids
+{
+    public class Asteroid
+    {
+        public Point location;
+        public int x;
+        public int y;
+        Pen pen = new Pen(Color.White);
+
+
+        public Asteroid(Point p, int xx, int yy)
+        {
+
+            location = new Point();
+            location = p;
+            x = xx;
+            y = yy;
+
+        }
+        public void move()
+        {
+            location.X += x;
+            location.Y += y;
+            if (location.X < 2) location.X = 500;
+            if (location.Y < 2) location.Y = 500;
+            if (location.X > 500) location.X = 1;
+            if (location.Y > 500) location.Y = 1;
+        }
+        public void draw(Graphics g)
+        {
+            g.DrawEllipse(pen, location.X, location.Y, 30, 30);
+
+        }
+    }
+}
---
+++ moovingasteroids/moovingasteroids/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moovingasteroids
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        List<Asteroid> asts;
+        Brush b = new SolidBrush(Color.Green);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            
+
+            asts = new List<Asteroid>();
+           
+            asts.Add(new Asteroid(new Point(200, 200), -5, 4));
+            asts.Add(new Asteroid(new Point(400, 200), -9, 4));
+            asts.Add(new Asteroid(new Point(300, 100), -2, 6));
+            asts.Add(new Asteroid(new Point(230, 130), -2, 2));
+            asts.Add(new Asteroid(new Point(330, 90), -2, 2));
+
+            
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.Black);
+            foreach (Asteroid a in asts)
+            {
+                a.move();
+                a.draw(g);
+                g.FillRectangle(b, 250, 250, 10, 10); // starship
+            }
+        }
+    }
+}
